daf0873f2a7af242c585c9173b6ffa34b338622f
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-02 21:34:59 +0000

    create the endpoints for the index, show, update, destroy, and profile methods in the UserController

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthenticationController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
 
+    Route::get('user', [UserController::class, 'index']);
+    Route::get('user/{user}', [UserController::class, 'show']);
+    Route::put('user/{user}', [UserController::class, 'update']);
+    Route::delete('user/{user}', [UserController::class, 'destroy']);
+    Route::get('profile', [UserController::class, 'profile']);
 
     Route::post('logout', [AuthenticationController::class, 'logout']);
 });
