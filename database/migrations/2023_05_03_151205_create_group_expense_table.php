abdd528b28d09961cbda38b17cf8ea38d5a7b83b
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-03 15:14:03 +0000

    define a migration for the join table

---
+++ database/migrations/2023_05_03_151205_create_group_expense_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('group_expense', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('group_id')->constrained()->onDelete('cascade');
+            $table->foreignId('expense_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+
+            $table->index(['group_id', 'expense_id']);
+            $table->unique(['group_id', 'expense_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('group_expense');
+    }
+};
