df569e8953b6bd0769c909c2e07ede9ab3880f44
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-03 09:42:39 +0000

    add routes for groups

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\API\AuthenticationController;
 use App\Http\Controllers\API\UserController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\UserGroupController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,14 +18,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
 
+    // User routes
     Route::get('user', [UserController::class, 'index']);
     Route::get('user/{user}', [UserController::class, 'show']);
     Route::put('user/{user}', [UserController::class, 'update']);
     Route::delete('user/{user}', [UserController::class, 'destroy']);
     Route::get('profile', [UserController::class, 'profile']);
 
+    // Group routes
+    Route::get('groups', [UserGroupController::class, 'index']);
+    Route::post('groups', [UserGroupController::class, 'store']);
+    Route::get('groups/{group}', [UserGroupController::class, 'show']);
+    Route::put('groups/{group}', [UserGroupController::class, 'update']);
+    Route::delete('groups/{group}', [UserGroupController::class, 'destroy']);
+
     Route::post('logout', [AuthenticationController::class, 'logout']);
 });
 
+// Authentication routes
 Route::post('register', [AuthenticationController::class, 'register']);
 Route::post('login', [AuthenticationController::class, 'login']);
