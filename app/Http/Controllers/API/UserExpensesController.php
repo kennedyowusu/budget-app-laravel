a5c8093870ba96f61d6018203a24ebff2b4d1d5e
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-03 13:32:10 +0000

    create controller for expenses

---
+++ app/Http/Controllers/API/UserExpensesController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ExpenseRequest;
+use App\Http\Resources\ExpenseResource;
+use App\Models\Expense;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class UserExpensesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $user = auth()->user()->load('expenses');
+        return ExpenseResource::collection($user->expenses);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ExpenseRequest $request)
+    {
+        try {
+            DB::beginTransaction();
+
+            $validatedData = $request->validated();
+            $validatedData['user_id'] = auth()->id();
+
+            $expense = Expense::create($validatedData);
+
+            DB::commit();
+
+            return ExpenseResource::make($expense);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+
+            Log::error($th->getMessage());
+            Log::error($request->validated());
+
+            return response()->json(['message' => 'Something went wrong'], 500);
+        } finally {
+            DB::commit();
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Expense $expense)
+    {
+        try {
+            if($expense->user_id !== auth()->id()) {
+                return response()->json(['message' => 'Unauthorized'], 401);
+            }
+
+            return ExpenseResource::make($expense);
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+            return response()->json(['message' => 'Something went wrong'], 500);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ExpenseRequest $request, Expense $expense)
+    {
+        try {
+            DB::beginTransaction();
+
+            if($expense->user_id !== auth()->id()) {
+                return response()->json(['message' => 'Unauthorized'], 401);
+            }
+
+            $validatedData = $request->validated();
+            $expense->update($validatedData);
+
+            DB::commit();
+
+            return ExpenseResource::make($expense);
+
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+            Log::error($request->validated());
+
+            return response()->json(['message' => 'Something went wrong'], 500);
+        } finally {
+            DB::commit();
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        try {
+            DB::beginTransaction();
+
+            if($user->id !== auth()->id()) {
+                return response()->json(['message' => 'Unauthorized'], 401);
+            }
+
+            $user->delete();
+
+            DB::commit();
+
+            return response()->json(['message' => 'User deleted'], 200);
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+
+            DB::rollBack();
+            return response()->json(['message' => 'Something went wrong'], 500);
+        } finally {
+            DB::commit();
+        }
+    }
+}
