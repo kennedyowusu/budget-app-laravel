aebfd0c8a407f90c8f4aa9106728aa336ad45a92
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-03 09:44:21 +0000

    create group model with its necessary associations

---
+++ app/Models/Group.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Group extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'icon', 'description', 'is_active', 'user_id'];
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->where('is_active', true);
+    }
+
+    public function scopeInactive($query)
+    {
+        return $query->where('is_active', false);
+    }
+}
